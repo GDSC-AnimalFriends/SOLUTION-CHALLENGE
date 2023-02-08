df98a08017c32c1ac36fcd79170376edcd2cbd3c
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-02-08 21:54:08 +0900

    Feat : 로그인 화면 ui 구성

--- lib/view/login/login_page.dart
+++ lib/view/login/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:solution_challenge/view/theme/app_text_theme.dart';
+import 'package:get/get.dart';
+
+import '../../routes/app_pages.dart';
 
 //예시용 페이지입니다
 class LoginPage extends StatelessWidget {
@@ -7,12 +9,104 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Text(
-            "로그인페이지",
-            style: inputHintStyle,
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(16, 90, 16, 0),
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.white,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  '앱 네임',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+                const Text(
+                  '소중한 사람, 확인해보세요',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+                const SizedBox(height: 42),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: '이메일',
+                    labelStyle: const TextStyle(color: Colors.black),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          width: 1, color: Theme.of(context).primaryColor),
+                      borderRadius: const BorderRadius.all(Radius.circular(15)),
+                    ),
+                    border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 12),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: '비밀번호',
+                    labelStyle: const TextStyle(color: Colors.black),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          width: 1, color: Theme.of(context).primaryColor),
+                      borderRadius: const BorderRadius.all(Radius.circular(15)),
+                    ),
+                    border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                  ),
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                ),
+                const SizedBox(height: 42),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    width: double.infinity,
+                    height: 65,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: const BorderRadius.all(Radius.circular(15)),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        '로그인',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 12),
+                GestureDetector(
+                  onTap: () {
+                    Get.toNamed(Routes.REGISTER_TYPE);
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 65,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                    child: Center(
+                      child: Text(
+                        '회원가입',
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/view/login/register/register_type_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class RegisterTypePage extends StatelessWidget {
+  const RegisterTypePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
