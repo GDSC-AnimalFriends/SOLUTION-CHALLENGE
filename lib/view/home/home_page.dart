a7ebca5841ebd333d0ed660de558d6a7adb7ee34
NOGUENmark2 <nhg1113@icloud.com>
2023-02-09 18:14:43 +0900

    Feat: home scrollable TabBar. 스와이프하여 넘길 수 있는 탭 바.

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:solution_challenge/view/home/home_subpages/today_todo_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return PageView();
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("AppName"),
+            bottom: const PreferredSize(
+              preferredSize: Size.fromHeight(30.0),
+              child: TabBar(
+                indicator: UnderlineTabIndicator(
+                  borderSide: BorderSide(
+                    width: 4,
+                    color: Colors.black,
+                  ),
+                ),
+                isScrollable: true,
+                indicatorColor: Colors.blue,
+                tabs: [
+                  Tab(child: Text("오늘의 할 일")),
+                  Tab(child: Text("전체 보기")),
+                  Tab(child: Text("알림")),
+                  Tab(child: Text("마이페이지")),
+                ],
+              ),
+            ),
+          ),
+          body: const TabBarView(
+            children: <Widget>[
+              TodayTodoPage(),
+              TodayTodoPage(),
+              TodayTodoPage(),
+              TodayTodoPage(),
+            ],
+          ),
+        ));
   }
 }
