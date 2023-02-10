18aedea4ff39473f439e8e56da8cc483cc0fed34
rio319-an <78468001+rio319-an@users.noreply.github.com>
2023-02-10 15:08:41 +0900

    Feat : 투두 페이지 이동

--- lib/view/login/login_base_widget.dart
+++ lib/view/login/login_base_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:solution_challenge/view/theme/app_colors.dart';
 
 class LoginBaseWidget extends StatelessWidget {
   final List<Widget> widgetList;
@@ -11,11 +12,11 @@ class LoginBaseWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
+      backgroundColor: white,
       body: SafeArea(
         child: Container(
           padding: const EdgeInsets.fromLTRB(16, 90, 16, 16),
           width: double.infinity,
-          color: Colors.white,
           child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
