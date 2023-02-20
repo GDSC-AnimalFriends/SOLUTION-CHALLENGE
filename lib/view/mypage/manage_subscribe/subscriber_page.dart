bb4b203e9fc447c1a887e7f9ff958b1fd4e560ef
yeogaeng <81527268+yeogaeng@users.noreply.github.com>
2023-02-21 03:09:54 +0900

    Feat: subscriber_page 구독자 listview

--- lib/view/mypage/manage_subscribe/subscriber_page.dart
+++ lib/view/mypage/manage_subscribe/subscriber_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:solution_challenge/view/common/appbar_only_back.dart';
-//import 'package:solution_challenge/view/common/common_button.dart';
-//import 'package:solution_challenge/view/theme/app_colors.dart';
+import 'package:solution_challenge/view/theme/app_colors.dart';
 
 class SubscriberManage extends StatefulWidget {
   const SubscriberManage({super.key});
@@ -13,6 +12,9 @@ class SubscriberManage extends StatefulWidget {
 class _SubscriberManageState extends State<SubscriberManage> {
 
   @override
+  final List<String> subscribers = <String>['여경','지수'];
+
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -33,7 +35,7 @@ class _SubscriberManageState extends State<SubscriberManage> {
                 ),
               ),
               Container(
-                height: 400,
+                height: 600,
                 width: 354,
                 decoration: BoxDecoration(
                   border: Border.all(
@@ -43,25 +45,65 @@ class _SubscriberManageState extends State<SubscriberManage> {
                 ),
                 child: Column(
                   children: [
-                    ListView(
-                      padding: EdgeInsets.all(8),
-                      children: [
-
-                      ],
-                    )
+                    subscriberListView(),
                   ],
-                ),
+                )
               )
             ],
-          )
-        ),
+          ),
+        )
       )
     );
   }
+
+  Widget subscriberListView() {
+    return Expanded(
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+          padding: EdgeInsets.all(8),
+          itemCount: subscribers.length,
+          itemBuilder: (BuildContext context, int index){
+            return Container(
+              height: 70,
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(9),
+                  color: lightPrimaryColor,
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    margin: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text('${subscribers[index]}'),
+                  Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    child: Container(
+                      height: 39,
+                      width: 95,
+                      margin: EdgeInsets.only(right: 12),
+                      decoration: BoxDecoration(
+                        color: primaryColor,
+                        borderRadius: BorderRadius.circular(9),
+                        boxShadow: [
+                          BoxShadow(color: Colors.grey.withOpacity(0.9), blurRadius: 5.0,spreadRadius: 1.5, offset: const Offset(1,3))
+                        ]
+                      ),
+                      child: Center(child: Text('권한 부여',style: TextStyle(color: Colors.white),)),
+                        ),
+                       ),
+              ]
+            )
+            );
+          }
+      ),
+    );
+  }
 }
-
-// class Person extends SubscriberManage{
-//   String? name;
-//   Person(this.name);
-// }
-
