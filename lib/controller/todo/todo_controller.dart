a6c6c025853a0a0429e778f8b98adba1b7c33477
Jisu <jsomnium@naver.com>
2023-02-25 21:29:58 +0900

    Style: 주간 반복 List 수정
    주간 반복 Bool 값들을 List로 해줬다

--- lib/controller/todo/todo_controller.dart
+++ lib/controller/todo/todo_controller.dart
@@ -2,45 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class TodoController extends GetxController {
+  ///////////////// ▼ 투두 관련 ▼
+
   final todoInput = TextEditingController(); // 투두
   final todoInfoInput = TextEditingController(); // 투두 설명
   Rx<DateTime> todoDate = DateTime.now().obs; // 투두 날짜
 
-  ///////////////// ▼ 반복
+  ///////////////// ▼ 반복 ▼
 
   RxBool RepeatEnabled = false.obs; // 반복 여부
 
   void RepeatEnable() {
+    // 반복 ON
     RepeatEnabled.value = true;
   }
 
   void RepeatDisable() {
+    // 반복 OFF
     RepeatEnabled.value = false;
   }
 
   RxBool RepeatEveryWeek = false.obs; // 매주 반복
   RxBool RepeatEveryDay = false.obs; // 매일 반복
 
-  RxBool RepeatMon = false.obs; // 월 반복
-  RxBool RepeatTue = false.obs; // 화 반복
-  RxBool RepeatWed = false.obs; // 수 반복
-  RxBool RepeatThr = false.obs; // 목 반복
-  RxBool RepeatFri = false.obs; // 금 반복
-  RxBool RepeatSat = false.obs; // 토 반복
-  RxBool RepeatSun = false.obs; // 일 반복
+  List<RxBool> WeekSelected = <RxBool>[
+    false.obs, // 월
+    false.obs, // 화
+    false.obs, // 수
+    false.obs, // 목
+    false.obs, // 금
+    false.obs, // 토
+    false.obs, // 일
+  ];
 
-  ///////////////// ▼ 알람
+  ///////////////// ▼ 알람 ▼
 
-  RxBool AlramEnabled = false.obs;
+  RxBool AlramEnabled = false.obs; // 알람 여부
 
   void AlramEnable() {
+    // 알람 ON
     AlramEnabled.value = true;
   }
 
   void AlramDisable() {
+    // 알람 OFF
     AlramEnabled.value = false;
   }
-  ///////////////// ▼ 뒤로가기
+  ///////////////// ▼ 뒤로가기 ▼
 
   void back() {
     Get.back();
