7b745466e7d470a3f7c49888db942a8fd867ba70
yeogaeng <81527268+yeogaeng@users.noreply.github.com>
2023-03-25 00:59:03 +0900

    Style: 주석

--- lib/controller/my_page/subscriber_manage_controller.dart
+++ lib/controller/my_page/subscriber_manage_controller.dart
@@ -24,8 +24,12 @@ class SubscriberManageController extends GetxController {
       ),
     );
   }
+  // 노인이면 구독자가 보호자들(uid) , 보호자면 구독자가 노인(uid)
+  // 본인 uid에 저장된 구독리스트(uid)를 조회하고 그 구독자(uid)에 맞는 img?, displayName, email, 권한설정
+  // 구독자 리스트 : 구독자 uid, 수정권한(bool)
 
   void toAddSubscirber() {
     Get.toNamed(Routes.SUBSCRIBE_ADD);
   }
+
 }
